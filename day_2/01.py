04225ddbdb029bf576b4a41770c69d448a019f18
ANMOL <sonianmol0918@gmail.com>
2026-07-05 10:22:24 +0530

    print function

--- day_2/01.py
+++ day_2/01.py
@@ -2,10 +2,13 @@
 #age is variable and 34 is no.
 
 age = 34 #data type is integer
-name = anmol #string
+name = "anmol" #string
 cgpa = 4.67 #float
 '''
 34age =77 #invalid
 age34 = 55 #valid 
 '''
 print (age)
+print("age")
+print('age') 
+print(name)
