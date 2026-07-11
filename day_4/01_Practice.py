97bc940e356320c31c743f8cd404868e3cf0ddfb
ANMOL <sonianmol0918@gmail.com>
2026-07-11 19:39:59 +0530

    day4_practice_questions

---
+++ day_4/01_Practice.py
@@ -0,0 +1,97 @@
+#Program that print 
+'''
+#Q1: Your First Program
+Write a program that prints:
+Hello, World! Welcome to Python.
+'''
+print("Hello, World! Welcome to Python\n")
+
+'''
+Q2: Print a Poem
+Write a program that prints the following poem using a single print() statement:
+(Hint: Use \n for a new line.)
+Twinkle, twinkle, little star,
+How I wonder what you are!
+'''
+print("Twinkle, twinkle, little star\nHow I wonder what you are!")
+
+
+'''
+Q3: Variables & Data Types
+Create variables to store:
+
+Your name (string)
+Your age (integer)
+Your height in meters (float)
+A boolean value representing whether you are a student
+Print all of them in one line.
+'''
+name = str(input("your_name\t"))
+age = int(input("your_age\t"))
+height = float(input("your_height\t"))
+
+is_student = True
+print("Name is \t"+name+"\nAge is\t\t",age,"\nHeight is\t",height)
+
+
+'''
+Q4: Typecasting Practice
+You are given a string:
+
+num = "45"
+
+Convert it into an integer
+Add 10 to it
+Print the result
+'''
+
+'''
+Q5: Taking User Input
+Write a program that:
+
+Asks the user for their favorite food.
+Prints:
+Wow! I also like <food>.
+'''
+
+'''+
+Q6: Simple Calculator
+Write a program that:
+
+Takes two numbers as input from the user.
+
+Prints their:
+
+Sum
+Difference
+Product
+Quotient
+
+'''
+
+
+'''
+Q7: Escape Sequences
+Print the following output using escape sequences:
+
+Hello "Python" World!
+This is on a new line.
+This is a tab →	    after tab.
+'''
+'''
+Q8: Operator Challenge
+Write a program that:
+
+Takes an integer as input from the user.
+Prints the square and cube of that number.
+Q9: Quick Quiz (True/False)
+Mark each as True or False:
+
+Python code must always end with a semicolon ;
+The # symbol is used for comments in Python
+"123" and 123 are the same in Python
+The * operator is used for multiplication
+\n creates a new line
+Variables in Python can start with numbers
+int("10") + 5 gives 15
+'''
