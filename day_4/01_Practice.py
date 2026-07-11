b6118f1ab41de88970a2dc6ef86847518ae92655
ANMOL <sonianmol0918@gmail.com>
2026-07-11 19:51:05 +0530

    day4_practice_question

--- day_4/01_Practice.py
+++ day_4/01_Practice.py
@@ -4,7 +4,7 @@
 Write a program that prints:
 Hello, World! Welcome to Python.
 '''
-print("Hello, World! Welcome to Python\n")
+#print("Hello, World! Welcome to Python\n")
 
 '''
 Q2: Print a Poem
@@ -13,7 +13,7 @@ Write a program that prints the following poem using a single print() statement:
 Twinkle, twinkle, little star,
 How I wonder what you are!
 '''
-print("Twinkle, twinkle, little star\nHow I wonder what you are!")
+#print("Twinkle, twinkle, little star\nHow I wonder what you are!")
 
 
 '''
@@ -26,40 +26,39 @@ Your height in meters (float)
 A boolean value representing whether you are a student
 Print all of them in one line.
 '''
-name = str(input("your_name\t"))
-age = int(input("your_age\t"))
-height = float(input("your_height\t"))
+#name = str(input("your_name\t"))
+#age = int(input("your_age\t"))
+#height = float(input("your_height\t"))
 
-is_student = True
-print("Name is \t"+name+"\nAge is\t\t",age,"\nHeight is\t",height)
+#is_student = True
+#print("Name is \t"+name+"\nAge is\t\t",age,"\nHeight is\t",height)
 
 
 '''
 Q4: Typecasting Practice
 You are given a string:
-
 num = "45"
-
 Convert it into an integer
 Add 10 to it
 Print the result
 '''
-
+#num = "45"
+#num = int(num)
+#print(num+10)
 '''
 Q5: Taking User Input
 Write a program that:
-
 Asks the user for their favorite food.
 Prints:
 Wow! I also like <food>.
 '''
+#food = input("Favourite_Food\t")
+#print("Favourite food = ",food)
 
-'''+
+'''
 Q6: Simple Calculator
 Write a program that:
-
 Takes two numbers as input from the user.
-
 Prints their:
 
 Sum
@@ -68,16 +67,20 @@ Product
 Quotient
 
 '''
-
-
+#num1 = float(input("Enter no. 1\t"))
+#num2 = float(input("Enter no. 2\t"))
+#print("SUM = ",num1 + num2)
+#print("DIFF = ",num1 - num2)
+#print("PROD = ",num1 * num2)
+#print("QUOT = ",num1 / num2)
 '''
 Q7: Escape Sequences
 Print the following output using escape sequences:
-
 Hello "Python" World!
 This is on a new line.
 This is a tab →	    after tab.
 '''
+print("Hello \"Python\" World!\nThis is on a new line.This is a tab →\t after tab.")
 '''
 Q8: Operator Challenge
 Write a program that:
