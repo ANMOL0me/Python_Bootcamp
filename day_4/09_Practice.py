a71219e87b40ac72bd46e69ddda76e033375528a
ANMOL <sonianmol0918@gmail.com>
2026-07-13 16:00:07 +0530

    day4_practice

--- day_4/09_Practice.py
+++ day_4/09_Practice.py
@@ -1,32 +1,8 @@
-''' 
-If-Else Conditional Statements
-Write a program that asks the user for a number and prints whether it is positive, negative, or zero.
-'''
-num = float(input("Enter a number\n"))
-if (num>=0):
-    print("num is positive")
-elif (num<0):
-    print("num is negative")
-else:
-    print("incorrect input")        
 
-'''
-Create a program that checks if a person is eligible to vote (age >= 18).
-'''
-age = int(input("Enter your age"))
-if age>=18:
-    print("you are eligible")
-else:
-    print("not eligible")
-        
-'''
-Write a program that takes a number from the user and prints "Even" if it is even, otherwise "Odd".
-'''
-while True:
-    num = float(input("enter a num"))
-    if ((num//2) == 0):
-        print("num is even")
-        continue
-    else:
-        print("num is odd")
-        continue    
+
+p = "*"
+x=str
+for i in range(1,5):
+    print(p)
+    x= p + p
+
