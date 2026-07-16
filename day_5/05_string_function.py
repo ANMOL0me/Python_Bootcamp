57b25357c639ecd6b1bc4f0e9345cd9278ff9ebd
ANMOL <sonianmol0918@gmail.com>
2026-07-16 21:39:18 +0530

    day5_string_function

--- day_5/05_string_function.py
+++ day_5/05_string_function.py
@@ -6,36 +6,3 @@ print(text.lower())
 print(text.title())
 print(text.capitalize())
 
-#remove whitespace
-#text = "hello world"
-print(text.strip())
-print(text.lstrip())
-print(text.rstrip())
-
-#Finding and Replacing
-print(text.find("hello"))
-print(text.replace("hello","world"))
-
-#splitting and joining
-txt = "apple.banana,mango"
-fruits = text.split(",")
-print(fruits)
-
-new_txt = " - ".join(fruits)
-print(new_txt)
-
-
-#string properties
- 
-str = "python123"
-print(str.isalpha())
-print(str.isdigit())
-print(str.isalnum())
-print(str.isspace())
-
-# length
-charac = "hello , python"
-print(len(charac))
-print(ord('A'))
-print(charac(65))
-
