efd22c72eaf632b56f02e1eccdb0887bb9bf705b
ANMOL <sonianmol0918@gmail.com>
2026-07-19 15:09:50 +0530

    day5_fstring

---
+++ day_5/06_fstring.py
@@ -0,0 +1,11 @@
+#string formating
+
+#template = "Hi anmol , how are you"
+template = "Hi {} , how are you"
+a  = "anmol"
+b = "dhruv"
+d = "lakshita"
+c = "akshita"
+
+s1 = template.format(a)
+print(s1)
