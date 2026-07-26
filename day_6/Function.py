6b7caadafb4d7fee06a9988305f12358c1230f40
ANMOL <sonianmol0918@gmail.com>
2026-07-26 16:40:33 +0530

    function

--- day_6/Function.py
+++ day_6/Function.py
@@ -1,9 +1,23 @@
-def add(a,b):
+def sum(a,b):
  return f"sum = {a+b}"
-def add(a,b):
+def sub(a,b):
  return f"sub = {a-b}"
-def add(a,b):
+def mul(a,b):
  return f"Mul = {a*b}"
-def add(a,b):
+def div(a,b):
  return f"Div = {a/b}"
-print("What operation you want to perform:\n1\tSum\n2\tSub\n3\tMul\n4\tDiv")
+a = float(input("Enter a num:\t"))
+b = float(input("Enter a num:\t"))
+print("\nWhat operation you want to perform:\n1\tSum\n2\tSub\n3\tMul\n4\tDiv")
+op = int(input("->\t"))
+match op:
+ case 1:
+  print(sum(a,b)) 
+ case 2:
+  print(sub(a,b))
+ case 3:
+  print(mul(a,b))
+ case 4:
+  print(div(a,b))
+ case _:
+  print("check input")
