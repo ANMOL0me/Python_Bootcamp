3ecb4c3d75d913eb0f2dc682fb1e29bbe25229a4
ANMOL <sonianmol0918@gmail.com>
2026-07-26 16:28:32 +0530

    function

--- day_6/Function.py
+++ day_6/Function.py
@@ -1,7 +1,9 @@
-a = int(input("Enter a num\n"))
-b = int(input("Enter a num\n"))
-print(add(a,b))
-
- def add(a,b):
-  return a+b
-
+def add(a,b):
+ return f"sum = {a+b}"
+def add(a,b):
+ return f"sub = {a-b}"
+def add(a,b):
+ return f"Mul = {a*b}"
+def add(a,b):
+ return f"Div = {a/b}"
+print("What operation you want to perform:\n1\tSum\n2\tSub\n3\tMul\n4\tDiv")
