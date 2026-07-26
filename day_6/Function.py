96a7f010895db9e485489d8f6da95a28a6d64f90
ANMOL <sonianmol0918@gmail.com>
2026-07-26 16:18:30 +0530

    function

--- day_6/Function.py
+++ day_6/Function.py
@@ -1,7 +1,7 @@
-a = int(input("Enter a num"))
-b = int(input("Enter a num"))
+a = int(input("Enter a num\n"))
+b = int(input("Enter a num\n"))
 print(add(a,b))
 
-def add(a,b):
- return a+b
+ def add(a,b):
+  return a+b
 
