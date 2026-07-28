5295226a925f369fa5f4786a81be30c8c3f56421
ANMOL <sonianmol0918@gmail.com>
2026-07-28 23:01:22 +0530

    practice

---
+++ day_7/practice.py
@@ -0,0 +1,10 @@
+'''1. Defining Functions
+>Write a function greet() that prints "Hello, Python Learner!" when called.'''
+def greet():
+ return "Hello,Python Learner! "
+print(greet())
+
+'''Write a function square(num) that returns the square of a given number. Test it with different numbers.'''
+def square(num):
+ return f"square={num*num}"
+print(square(3))
