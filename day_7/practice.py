8e3806a33a24a7c10c65e99715c0b2605c9347e3
ANMOL <sonianmol0918@gmail.com>
2026-07-28 23:04:38 +0530

    practice_function and arguments

--- day_7/practice.py
+++ day_7/practice.py
@@ -8,3 +8,18 @@ print(greet())
 def square(num):
  return f"square={num*num}"
 print(square(3))
+
+
+'''2. Function Arguments & Return Values
+Write a function full_name(first, last) that takes first name and last name as parameters and returns a single string in the format "First Last".'''
+
+def full_name(first,last):
+ print(f"{first} {last}")
+full_name("anmol","soni")
+ 
+
+'''
+Write a function calculate_area(length, width=10) that returns the area of a rectangle. Test it by calling the function with:
+
+Both length and width
+Only length (use default width)'''
