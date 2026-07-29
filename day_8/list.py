67002dd056a8cc5538a8dcf5f69824ecd8a7a331
ANMOL <sonianmol0918@gmail.com>
2026-07-29 23:10:16 +0530

    list methods

--- day_8/list.py
+++ day_8/list.py
@@ -9,10 +9,22 @@ my_list.remove(3)
 my_list.pop() #remove last element
 my_list.reverse()
 my_list.sort()
-'''
+
 
 my_list = [1,2,4,6]
-my_list.append(8)
+my_list.append(8) 
 print(my_list)
 my_list.insert(8,9)
 print(my_list)
+'''
+'''
+#table of 2
+table = []
+for i in range(0,11):
+ table.append(2*i)
+print(table)
+'''
+# list comprehension
+
+table = [2*i for i in range(0,11)]
+print(table)
