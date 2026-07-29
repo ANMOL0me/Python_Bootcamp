b9b1686d839065bcdb92df1b5733f2958f87ca6c
ANMOL <sonianmol0918@gmail.com>
2026-07-29 22:42:09 +0530

    Lists

--- day_8/list.py
+++ day_8/list.py
@@ -1,13 +1,18 @@
-# list
+'''# list
 my_list = [1,2,3,4]
 mixed = [10,"hello",3.14]
 # list method
-#my_list.append(4)
+my_list.append(4)
+print(my_list)
 my_list.insert(4,44) #duplicate files dont show
 my_list.remove(3) 
 my_list.pop() #remove last element
 my_list.reverse()
 my_list.sort()
+'''
 
-
+my_list = [1,2,4,6]
+my_list.append(8)
+print(my_list)
+my_list.insert(8,9)
 print(my_list)
