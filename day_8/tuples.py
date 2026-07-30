28bedb220a91226013a3cb83d198fce2a22c5e66
ANMOL <sonianmol0918@gmail.com>
2026-07-30 21:30:19 +0530

    tuples prop

---
+++ day_8/tuples.py
@@ -0,0 +1,11 @@
+#tuple
+'''
+a = (3,4,5,6)
+print(a)
+a[1] = 2
+b = (2,) #single element cnanot be used 
+'''
+
+my_tuple = (1, 2, 2, 3, 4)
+print(my_tuple.count(3))  # Output: 2
+print(my_tuple.index(3))   # Output: 3
