00c274a1b4e93f229f112c2283aca4bd091d8ce8
ANMOL <sonianmol0918@gmail.com>
2026-07-30 23:25:19 +0530

    sets

---
+++ day_8/sets.py
@@ -0,0 +1,16 @@
+my_set = {2,34,5,32,23}
+a = {2,34,2}
+print(my_set)
+print(a)
+print("my_set.add(4) =",my_set.add(4))
+                                    
+print("my_set.remove(34) =",my_set.remove(34))
+                                                                      
+                                                         
+print("my_set.pop() =",my_set.pop())        
+        
+print("my_set.union(a) =",my_set.union(a))   
+                             
+print("my_set.intersection(a) =",my_set.intersection(a))                           
+
+#print("my_set.diffrence(a)=",my_set.diffrence(a))
