f7acbbb623371842411c1f9528fd1114babe164c
ANMOL <sonianmol0918@gmail.com>
2026-07-30 21:31:08 +0530

    tuples prop

--- day_8/tuples.py
+++ day_8/tuples.py
@@ -7,5 +7,5 @@ b = (2,) #single element cnanot be used
 '''
 
 my_tuple = (1, 2, 2, 3, 4)
-print(my_tuple.count(3))  # Output: 2
+print(my_tuple.count(3))  # Output: 1
 print(my_tuple.index(3))   # Output: 3
