1c734266dc27d655afe6cba984d34f2221202518
ANMOL <sonianmol0918@gmail.com>
2026-08-04 22:48:28 +0530

    dictionaries methhod

---
+++ day_8/Practice/Dictionaries_method.py
@@ -0,0 +1,25 @@
+'''. Dictionaries and Dictionary Methods
+Create a dictionary student = {"name": "John", "age": 20, "grade": "A"} and:
+
+Print the value of "name".
+Change "grade" to "A+".
+Add a new key "city" with value "Delhi".
+Create a dictionary of three friends and their phone numbers. Use:
+
+keys() to get all names
+values() to get all numbers
+items() to loop over key-value pairs and print them'''
+'''
+student = {"name":"john","age":20,"grade":"A"}
+print(student['name'])
+student['grade'] = 'A+'
+print(student['grade'])
+student['city'] = 'Delhi'
+print(student)
+'''
+details = {"fr1":123,"fr2":345,"fr3":678}
+print(details.keys(),"\n")
+print(details.values(),"\n")
+print(details.items())
+
+
