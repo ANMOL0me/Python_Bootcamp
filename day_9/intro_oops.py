1156accb1f793fe5a42b1b96be0ae93a73182155
ANMOL <sonianmol0918@gmail.com>
2026-08-06 19:57:15 +0530

    day_9

---
+++ day_9/intro_oops.py
@@ -0,0 +1,11 @@
+#class , name , age ,elective, f-m name
+class form:
+ name = "anmol"
+ age = 20
+ def __init__():
+  print("constructor")
+ def out():
+  print(f"name = {name}\nage = {age}")
+
+elective = form
+print(elective.out())
