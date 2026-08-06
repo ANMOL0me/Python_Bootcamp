a848fd83278cfe8b08063bfcb45608762069ee3a
ANMOL <sonianmol0918@gmail.com>
2026-08-06 20:12:03 +0530

    practice

--- day_8/Practice/uncomplete.py
+++ day_8/Practice/uncomplete.py
@@ -7,14 +7,8 @@ print(nums.union(numss))
 '''Given a dictionary of products and their prices, find the product with the highest price'''
 
 product = {"apple":33, "mango":44,"banana":55}
-ss = list(product.values())
-j=0
-i=0
-while ss[i] >= ss[j]:
- j+=1
- print(ss[i])
+print(product.values())
  
-  
   
    
   
