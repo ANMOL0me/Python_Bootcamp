d0b64b6cae16c7ead0e37cdc491bb2d3ccc254b5
ANMOL <sonianmol0918@gmail.com>
2026-08-11 20:08:17 +0530

    Inheritance

--- day_9/inheritance.py
+++ day_9/inheritance.py
@@ -1,20 +1,25 @@
 class parent_class:
- data = "#453"
+def show(self): #overdiding
+  
+  print(f"{self.data}")
+ 
+
+
+class child(parent_class):  #inheritance
+data = "#453"
  def __init__(self):
   print("Want to see data\nY or N\n")
   x = input()
   if(x=='Y' or x=='y'):
    self.show()
   else:
-   EXIT 
+   pass 
  def show(self):
-  print(f"{self.data}")
-class child(parent_class):
- def skip_show(self):
-  print(f"{self.data}")
+  super().show()
+  print("vip method") 
 obj = child()
-obj.skip_show()
-show().parent_class()
+obj.show()
+
     
   
   
