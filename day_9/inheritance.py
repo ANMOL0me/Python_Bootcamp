78c18b4fa189539d9a7c0334f5f715d225045f37
ANMOL <sonianmol0918@gmail.com>
2026-08-11 18:56:11 +0530

    inheritance

--- day_9/inheritance.py
+++ day_9/inheritance.py
@@ -1,12 +1,20 @@
-class animal:#parent class
- name = "kkk"
- def __init__(self,name):
-  self.name = name
- def speak(self):
-  print(""sound of animal)
-class dog(animal):
- def speak(self):
-  print("woof!")
-d = dog
-d.speak()
-d.speak(animal)
+class parent_class:
+ data = "#453"
+ def __init__(self):
+  print("Want to see data\nY or N\n")
+  x = input()
+  if(x=='Y' or x=='y'):
+   self.show()
+  else:
+   EXIT 
+ def show(self):
+  print(f"{self.data}")
+class child(parent_class):
+ def skip_show(self):
+  print(f"{self.data}")
+obj = child()
+obj.skip_show()
+show().parent_class()
+    
+  
+  
