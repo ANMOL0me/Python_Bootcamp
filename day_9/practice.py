5b0c1f7bec0b52983418b5c6a954629b4cf03cb3
ANMOL <sonianmol0918@gmail.com>
2026-08-12 22:57:32 +0530

    practice question 3

--- day_9/practice.py
+++ day_9/practice.py
@@ -6,3 +6,26 @@ class car:
   return "Car is moving"
 obj = car()
 print(obj.drive())
+
+'''Create a class Person with a constructor (__init__) that accepts name and age as arguments and stores them as instance attributes.
+Create an object and print the person’s name and age.'''
+
+class Person:
+ def __init__(self,name,age):
+  self.age = age
+  self.name = name
+  print(f"name = {name},\nage = {age}")
+obj1 = Person("Anmol",21)
+
+'''Create a base class Animal with a method sound() that prints "Some sound".
+Create a derived class Dog that overrides sound() to print "Bark!".
+Create an object of Dog and call sound().'''
+
+class Animal:
+ def sound():
+  return "Some sound"
+class Dog(Animal):
+ def sound():
+  return "BArk!"
+obj2 = Dog
+print(obj2.sound())
