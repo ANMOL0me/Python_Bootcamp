86daeeab7066a105374607cc23b4ffa3578b311b
ANMOL <sonianmol0918@gmail.com>
2026-08-15 21:07:00 +0530

    project

---
+++ Vehicle_Managnment_System/Vehicle.py
@@ -0,0 +1,22 @@
+class Vehicle:
+ def vehiclenumber(self,vno):
+  self.vno = vno
+  return vno
+ def brand(self,vbrand):
+  self.vbrand = vbrand
+  return vbrand
+ def model(self,vmodel):
+  self.vmodel = vmodel
+  return vmodel
+ def price(self,vprice):
+  self.vprice = vprice
+  return vprice
+ def speed(self,vspeed):
+  self.vspeed = vspeed
+  return vspeed
+ def fueltype(self,vfueltype):
+  self.vfueltype = vfueltype
+  return vfueltype
+
+
+
