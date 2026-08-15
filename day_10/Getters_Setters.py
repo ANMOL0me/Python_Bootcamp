c36fb8af70b2a6076e8fa468fe99dc8b2d458e82
ANMOL <sonianmol0918@gmail.com>
2026-08-15 16:06:30 +0530

    property of decorators

--- day_10/Getters_Setters.py
+++ day_10/Getters_Setters.py
@@ -1,12 +1,21 @@
-class Brand:
- def __init__(self,name,amt):
+class Employee:
+ def __init__(self,name,salary):
   self.name = name
-  self.amt = amt
- def fname(self):
-  l  = self.name.split(" ")
-  print(l)
+  self.salary = salary
+ 
+ @property
+ def first_name(self):
+  l = self.name.split(" ")
   return l[0]
+  
+ @first_name.setter
+ def first_name(self,first):
+  l = self.name.split(" ")
+  new_name = f"{first} {l[1]}"
+  self.name = new_name
 
-e = Brand("Jack Daniels",3400)
-print(e.fname())
-print(e.chngname())
+e = Employee("Jack Doe",3455)
+
+print(e.first_name)
+e.first_name = "John"
+print(e.name)
