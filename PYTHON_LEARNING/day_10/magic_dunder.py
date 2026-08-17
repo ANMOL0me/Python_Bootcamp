86c319d577031eaf2c7371ac04bcae2e61982c94
ANMOL <sonianmol0918@gmail.com>
2026-08-17 19:07:18 +0530

    Python learning

---
+++ PYTHON_LEARNING/day_10/magic_dunder.py
@@ -0,0 +1,16 @@
+#dunder = double underscore
+class Employee:
+ def __init__(self,name,salary):#magic method
+  self.name = name
+  self.salary = salary
+ 
+ def __str__(self): #dunder
+  return f"name is {self.name}"
+  
+ def __repr__(self):
+  return f"{self.name}" 
+
+e = Employee("ssss",43222)
+print(e.name , e.salary)
+print(str(e))
+print(repr(e))
