f5d1363db9da9e50fa6cdacc62e11129afcabe06
ANMOL <sonianmol0918@gmail.com>
2026-08-28 22:57:36 +0530

    external module

--- PYTHON_LEARNING/Ext_modules_day_12/Request_Module.py
+++ PYTHON_LEARNING/Ext_modules_day_12/Request_Module.py
@@ -1,5 +1,7 @@
+#get request
+
 import requests
-r = requests.get('https://api.github.com/users/ANMOL0me')
+r = requests.post('https://api.github.com/users/ANMOL0me',data ={"login":"ANMOL0me"})
 print(r.text)
-with open("Anmol0me.txt","w") as f :
+with open("Anmol0meupdated.txt","w") as f :
     f.write(r.text)
