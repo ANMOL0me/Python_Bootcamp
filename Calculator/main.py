c2e103095efaabef17c0589c8b3ac4b47debd536
ANMOL <sonianmol0918@gmail.com>
2026-09-02 00:06:41 +0530

    calculator

---
+++ Calculator/main.py
@@ -0,0 +1,27 @@
+import operation
+try:
+ while(1): 
+   n1 = input("Enter a num =")
+   while(n1=="exit"):
+    exit() 
+   op = input("operation  = ")
+   while(op=="exit"):
+    exit()
+   n2 = input("Enter a num =")
+   while(n2 =="exit"):
+    exit()
+   n1 = int(n1)
+   n2 = int(n2)
+   match op:
+    case "+":
+     print(operation.Add(n1,n2))
+    case "-":
+     print(operation.Sub(n1,n2))
+    case "*":
+     print(operation.Mul(n1,n2))
+    case "/":
+     print(operation.Div(n1,n2))
+    case _:
+     print("Invalid operation")
+except Exception as e:
+ print("e")
---
+++ Calculator/operation.py
@@ -0,0 +1,8 @@
+def Add(n1,n2):
+ return f"Sum = {n1+n2}\n"
+def Sub(n1,n2):
+ return f"Sub = {n1-n2}\n"
+def Mul(n1,n2):
+ return f"Mul = {n1*n2}\n"
+def Div(n1,n2):
+ return f"Div = {n1/n2}\n"
